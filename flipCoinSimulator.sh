89954f579603c669c24440c5128b0667935e2c8a
namdev1994 <n.lavhare1994@gmail.com>
2021-03-31 13:27:56 +0530

    Added:No of times Winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,7 +1,17 @@
-randm=$((RANDOM%2))
-if [ $randm -eq 1 ]
-then
-	echo "Head As a Winner"
-else
-	echo "Tail As a Winner"
-fi
+echo -e "enter the times to toss coin \c"
+read num
+Head=0
+Tail=0
+
+for ((i=0; i<$num; i++))
+do
+	random=$((RANDOM%2))
+	if [ $random -eq 1 ]
+	then
+		 ((Head++))
+	else
+		 ((Tail++))
+	fi
+done
+echo "Head Wins for $Head times"
+echo "Tails wins for $Tail times"
